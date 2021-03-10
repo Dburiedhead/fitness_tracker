d5136b7ce43967b6d7dd7b51d48e84920de459aa
Dianka-ea <dmsoumare@sooja.fr>
2021-03-10 18:10:42 +0100

    started user profile

--- tracker/src/components/profile.js
+++ tracker/src/components/profile.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import { Row, Col, Button, Modal, Container, Card, Image, Accordion, Table } from 'react-bootstrap'
+import React from 'react';
+import { Row, Col, Button, Container } from 'react-bootstrap'
 
-class Profile extends Component {
-    render() {
+function edit() {
+    console.log('edit the field !')
+};
 
-        return (
-            <div>
-                <h1>Profile page</h1>
-                <p>Edit your profile, delete your account</p>
-                <Container style={{ paddingTop: '2%', background: 'rgb(255, 255, 255)' }}>
-                    <Row>
-                        <Col md={10}>
-                            
-                        </Col>
-                        <Col md={2}>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        )
-    }
+/*function Fields () {
+
+};*/
+
+function Profile() {
+    return (
+        <div>
+            <h1>Profile page</h1>
+            <p>Edit your profile, delete your account</p>
+            <Container style={{ paddingTop: '2%', background: 'rgb(255, 255, 255)' }}>
+                <Row>
+                    <Col md={10}>
+                    {/* <Fields /> */}
+                    </Col>
+                    <Col md={2}>
+                        <Button variant='primary' onClick={edit}>Edit your profile</Button>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
 }
   
 export default Profile;
